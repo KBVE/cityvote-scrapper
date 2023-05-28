e8836daf0699822514601d39ebad35ffdbcec078
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-05-28 01:13:03 -0400

    Sync

--- app/src/puppeteer/shaco.js
+++ app/src/puppeteer/shaco.js
@@ -4,14 +4,63 @@ const _v = require("../../log");
 //const vm = require('node:vm');
 
 
-async function clone(__website) {
-  _v(`Shaco Cloning ${__website}`);
+//?   Minimal Args by Jon
+const minimal_args = [
+  '--autoplay-policy=user-gesture-required',
+  '--disable-background-networking',
+  '--disable-background-timer-throttling',
+  '--disable-backgrounding-occluded-windows',
+  '--disable-breakpad',
+  '--disable-client-side-phishing-detection',
+  '--disable-component-update',
+  '--disable-default-apps',
+  '--disable-dev-shm-usage',
+  '--disable-domain-reliability',
+  '--disable-extensions',
+  '--disable-features=AudioServiceOutOfProcess',
+  '--disable-hang-monitor',
+  '--disable-ipc-flooding-protection',
+  '--disable-notifications',
+  '--disable-offer-store-unmasked-wallet-cards',
+  '--disable-popup-blocking',
+  '--disable-print-preview',
+  '--disable-prompt-on-repost',
+  '--disable-renderer-backgrounding',
+  '--disable-setuid-sandbox',
+  '--disable-speech-api',
+  '--disable-sync',
+  '--hide-scrollbars',
+  '--ignore-gpu-blacklist',
+  '--metrics-recording-only',
+  '--mute-audio',
+  '--no-default-browser-check',
+  '--no-first-run',
+  '--no-pings',
+  '--no-sandbox',
+  '--no-zygote',
+  '--password-store=basic',
+  '--use-gl=swiftshader',
+  '--use-mock-keychain',
+];
 
+
+async function load() {
+  
   const browser = await puppeteer.launch({
-    headless: "new",
+    headless: false,
     ignoreHTTPSErrors: true,
     args: ["--no-sandbox"],
   });
+
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1280, height: 720 });
+
+  
+}
+
+
+async function clone(__website) {
+  _v(`Shaco Cloning ${__website}`);
 
   let page = await browser.newPage();
 
